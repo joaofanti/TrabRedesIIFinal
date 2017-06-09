6792cc2b7c4b5b37a6fc6166c40f85ad4e5a349c
João Otávio Fanti de Souza <joaofantisouza@hotmail.com>
2017-06-09 18:50:22 -0300

    Create server.py

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
+from socket import *
+import struct
 
+
+s = socket(AF_INET, SOCK_RAW, IPPROTO_UDP); # Abrindo o socket
+
+while(True):
+	response, addr = s.recvfrom(4711);
+	print len(response)
+	response_id = struct.unpack('!HHHHHHHHHHHHHHHHHH', response);
+	print response_id
